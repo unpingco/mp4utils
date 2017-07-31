ba7d7d54d4d0d7c9899af23cbbbf388dc9b9385a
unpingco <unpingco@dev>
2017-07-31 10:55:30 -0700

    added split by junk option for mp4_cut.py

--- mp4_cut.py
+++ mp4_cut.py
@@ -1,6 +1,20 @@
-from datetime import datetime
-from subprocess import PIPE, call
-import tempfile, os, argparse
+from datetime import datetime, timedelta
+from subprocess import PIPE, call, Popen
+import tempfile, os, argparse, sys, re
+
+def get_file_duration(infilename):
+   '''
+   :param infilename: input mp4 filename
+   :type infilename: str
+   :returns: (h,m,s) tuple 
+   '''
+   cmd=['ffmpeg','-i',infilename]
+   p=Popen(cmd,stdout=PIPE,stderr=PIPE)
+   output=p.stderr.read()
+   match=re.search('Duration: (.*?)\.',output)
+   match.groups()
+   h,m,s= parse_ts(match.groups()[0])
+   return datetime(2017,1,1,h,m,s)
 
 def parse_ts(instring):
    x=instring.split(':')
@@ -12,6 +26,13 @@ def parse_ts(instring):
 def format_ts(instring):
    h,m,s=parse_ts(instring)
    return '%02d:%02d:%02d'%(h,m,s)
+
+def run_cmd_dt(start,end,infname,outfname):
+   assert isinstance(start,datetime)
+   assert isinstance(end,datetime)
+   start_time='%02d:%02d:%02d'%(start.hour,start.minute,start.second)
+   end_time='%02d:%02d:%02d'%(end.hour,end.minute,end.second)
+   run_cmd(start_time,end_time,infname,outfname)
 
 def run_cmd(start='00:00:00',end='23:00:00',infname='foo.mp4',outfname='outfoo.mp4'):
    duration = get_duration(start,end)
@@ -69,6 +90,10 @@ The two complementary parts are joined to make the output file.''')
                            help='hh:mm:ss',
                            default='',
                            )
+   parser.add_argument("-c","--chunk_duration", 
+                           help='Divide into <n> chunks of this duration hh:mm:ss. Overrides other flags!',
+                           default='',
+                           )
    parser.add_argument("--invert", 
                         dest='invert',
                         default=False,
@@ -76,6 +101,22 @@ The two complementary parts are joined to make the output file.''')
                         help="return complement of indicated section")
 
    args = parser.parse_args()
+   if args.chunk_duration:
+      # this over-rides other options
+      hc,mc,sc=parse_ts(args.chunk_duration)
+      start_time=datetime(2017,1,1,0,0,0)
+      end_time=datetime(2017,1,1,hc,mc,sc)
+      file_length = get_file_duration(args.infile)
+      dt = timedelta(hours=hc,minutes=mc,seconds=sc)
+      outfilename_head = args.outfile.replace('.mp4','')
+      n=0
+      while end_time < file_length:
+         run_cmd_dt(start_time,end_time,args.infile,'%s_%03d.mp4'%(outfilename_head,n))
+         start_time = end_time
+         end_time = start_time + dt
+         n += 1
+      sys.exit()
+
    duration = get_duration(args.start_time,args.end_time)
    if args.invert: 
       if args.start_time=='00:00:00': # tail section
